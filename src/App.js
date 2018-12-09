b6e0851d8cd0fefb92581ca9097f9a0ffe7c3976
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-09 13:54:28 +0200

    Added function to load the Maps JavaScript API using a script tag.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    initMap = () => {
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+
     render() {
         return (
             <main>
@@ -10,6 +18,15 @@ class App extends Component {
             </main>
         );
     }
+}
+
+function loadScript(url) {
+    var index = window.document.getElementsByTagName("script")[0]
+    var script = window.document.createElement("script")
+    script.src = url
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
 }
 
 export default App;
