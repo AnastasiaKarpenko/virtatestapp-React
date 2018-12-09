8cb80ac1a3314fb06427d731fbcdee3d81b3162f
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-09 13:59:31 +0200

    Added script url with api key to load script, made google difined by calling window first to create new map

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 
 class App extends Component {
 
+    renderMap = () => {
+        loadScript("https://maps.googleapis.com/maps/api/js?key=" +
+            "AIzaSyAAHhlH1VNV94TBEO4wLnaJvNaw5r6W9sk&callback=initMap")
+    }
+
     initMap = () => {
-        const map = new google.maps.Map(document.getElementById('map'), {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: -34.397, lng: 150.644},
             zoom: 8
         });
@@ -21,12 +26,12 @@ class App extends Component {
 }
 
 function loadScript(url) {
-    var index = window.document.getElementsByTagName("script")[0]
-    var script = window.document.createElement("script")
-    script.src = url
-    script.async = true
-    script.defer = true
-    index.parentNode.insertBefore(script, index)
+    var index = window.document.getElementsByTagName("script")[0];
+    var script = window.document.createElement("script");
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    index.parentNode.insertBefore(script, index);
 }
 
 export default App;
